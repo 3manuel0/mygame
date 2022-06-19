27f7f2089dbae1900142784ef7df2c1a63a7903e
unknown <saiddimension@gmail.com>
2022-06-19 15:01:08 +0100

    player movement fixed

--- script.js
+++ script.js
@@ -30,12 +30,27 @@ class Player {
   }
 }
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 player.draw();
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 animate();
 
@@ -43,15 +58,20 @@ addEventListener("keydown", ({ keyCode }) => {
   console.log(keyCode);
   switch (keyCode) {
     case 87:
-      player.velocity.y -= 20;
-      console.log("up");
+      if (
+        player.position.y + player.height + player.velocity.y >=
+        canvas.height
+      ) {
+        player.velocity.y = -20;
+        console.log("up");
+      }
       break;
     case 68:
-      player.velocity.x += 5;
+      keys.right.pressed = true;
       console.log("right");
       break;
     case 65:
-      player.velocity.x -= 5;
+      keys.left.pressed = true;
       console.log("right");
       break;
   }
@@ -60,14 +80,13 @@ addEventListener("keyup", ({ keyCode }) => {
   console.log(keyCode);
   switch (keyCode) {
     case 87:
-      console.log("up");
       break;
     case 68:
-      player.velocity.x = 0;
+      keys.right.pressed = false;
       console.log("right");
       break;
     case 65:
-      player.velocity.x = 0;
+      keys.left.pressed = false;
       console.log("right");
       break;
   }
